80221d7bbcd574c485fde05987066faf45b7c112
chenpeng <chenpeng@ccbft.com>
2024-10-22 22:07:22 +0800

    add workload spec and status

---
+++ api/core/types.go
@@ -0,0 +1,52 @@
+package core
+
+import (
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+// ConditionStatus defines conditions of resources
+type ConditionStatus string
+
+const (
+	ConditionTrue    ConditionStatus = "True"
+	ConditionFalse   ConditionStatus = "False"
+	ConditionUnknown ConditionStatus = "Unknown"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type ObjectReference struct {
+	// +optional
+	Kind string
+	// +optional
+	Namespace string
+	// +optional
+	Name string
+	// +optional
+	UID types.UID
+	// +optional
+	APIVersion string
+	// +optional
+	ResourceVersion string
+
+	FieldPath string
+}
+
+// ResourceName is the name identifying various resources in a ResourceList.
+type ResourceName string
+
+const (
+	// CPU, in cores. (500m = .5 cores)
+	ResourceCPU ResourceName = "cpu"
+	// Memory, in bytes. (500Gi = 500GiB = 500 * 1024 * 1024 * 1024)
+	ResourceMemory ResourceName = "memory"
+	// Volume size, in bytes (e,g. 5Gi = 5GiB = 5 * 1024 * 1024 * 1024)
+	ResourceStorage ResourceName = "storage"
+	// Local ephemeral storage, in bytes. (500Gi = 500GiB = 500 * 1024 * 1024 * 1024)
+	// The resource name for ResourceEphemeralStorage is alpha and it can change across releases.
+	ResourceEphemeralStorage ResourceName = "ephemeral-storage"
+)
+
+type ResourceList map[ResourceName]resource.Quantity
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
--- api/v1/workload_types.go
+++ api/v1/workload_types.go
@@ -17,25 +17,33 @@ limitations under the License.
 package v1
 
 import (
+	"cloudkube/polykube/api/core"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // WorkloadSpec defines the desired state of Workload
 type WorkloadSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Type WorkloadType
 
-	// Foo is an example field of Workload. Edit workload_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// k8s raw definitions if type is k8s-object
+	K8SObject []runtime.RawExtension
+
+	DestAffinity *Affinity
+
+	SchedulerName string
+
+	CheckFilters []K8SObject
 }
 
 // WorkloadStatus defines the observed state of Workload
 type WorkloadStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Phase              WorkloadPhase
+	ObservedGeneration int64
+	Conditions         []WorkloadCondition
+	K8SObjectStatus    []K8SObjectStatus
+
+	AllocatedResources core.ResourceList
 }
 
 //+kubebuilder:object:root=true
@@ -48,6 +56,99 @@ type Workload struct {
 
 	Spec   WorkloadSpec   `json:"spec,omitempty"`
 	Status WorkloadStatus `json:"status,omitempty"`
+}
+
+type WorkloadType string
+
+const (
+	K8SOBJECT WorkloadType = "k8s-object"
+)
+
+type WorkloadPhase string
+
+const (
+	WorkloadPending  WorkloadPhase = "Pending"
+	WorkloadCreating WorkloadPhase = "Creating"
+	WorkloadRunning  WorkloadPhase = "Running"
+	WorkloadUnknown  WorkloadPhase = "Unknown"
+)
+
+// Affinity is a group of affinity scheduling rules.
+type Affinity struct {
+	WorkloadAffinity     *WorkloadAffinity
+	WorkloadAntiAffinity *WorkloadAntiAffinity
+}
+
+type WorkloadAffinity struct {
+	RequiredDuringSchedulingIgnoredDuringExecution  []WorkloadAffinityTerm
+	PreferredDuringSchedulingIgnoredDuringExecution []WeightedWorkloadAffinityTerm
+}
+
+type WorkloadAntiAffinity struct {
+	RequiredDuringSchedulingIgnoredDuringExecution  []WorkloadAffinityTerm
+	PreferredDuringSchedulingIgnoredDuringExecution []WeightedWorkloadAffinityTerm
+}
+
+type WorkloadAffinityTerm struct {
+	LabelSelector     *metav1.LabelSelector
+	Namespaces        []string
+	TopologyKey       string
+	NamespaceSelector *metav1.LabelSelector
+}
+
+type WeightedWorkloadAffinityTerm struct {
+	// range 1-100
+	Weight               int32
+	WorkloadAffinityTerm WorkloadAffinityTerm
+}
+
+type K8SObject struct {
+	metav1.GroupVersionKind
+	Name string
+}
+
+type WorkloadConditionType string
+
+// These are valid conditions of a workload.
+const (
+	WorkloadScheduled WorkloadConditionType = "WorkloadScheduled"
+	WorkloadReady     WorkloadConditionType = "Ready"
+)
+
+// WorkloadCondition describes the state of a workload at a certain point.
+type WorkloadCondition struct {
+	// Type of replica set condition.
+	Type WorkloadConditionType
+	// Status of the condition, one of True, False, Unknown.
+	Status core.ConditionStatus
+	// The last time the condition transitioned from one status to another.
+	// +optional
+	LastTransitionTime metav1.Time
+	// The reason for the condition's last transition.
+	// +optional
+	Reason string
+	// A human readable message indicating details about the transition.
+	// +optional
+	Message string
+}
+
+type K8SObjectStatus struct {
+	Reference core.ObjectReference
+
+	// Scheduled specifies whether the resource is scheduled.
+	Scheduled bool
+
+	// Ready specifies whether the resource is ready.
+	Ready bool
+
+	// Deleted
+	Deleted bool
+
+	// Replicas
+	Replicas int32
+
+	// ReadyReplicas
+	ReadyReplicas int32
 }
 
 //+kubebuilder:object:root=true
